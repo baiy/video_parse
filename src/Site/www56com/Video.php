6783d89b06dbe813569595fe44f0168bc1f20e82
taotao.chen <taotao.chen@yulore.com>
2016-05-20 16:21:16 +0800

    添加56网

---
+++ src/Site/www56com/Video.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 抓取56万视频
+ */
+namespace VideoParse\Site\www56com;
+
+class Video extends \VideoParse\Site\Video
+{
+    /**
+     * 获取播放地址
+     * @return array
+     */
+    public function GetPlayUrl()
+    {
+        $play_url = 'http://share.vrs.sohu.com/my/v.swf&topBar=1&id={id}&autoplay=false&from=page';
+        $response = \Unirest\Request::get($this->url);
+        $html     = $response->body;
+        preg_match("#vid: '(?<id>.*?)',#", $response->body, $m);
+        if (!empty($m['id'])) {
+            return array('swf' => str_replace('{id}', $m['id'], $play_url));
+        }
+        return false;
+    }
+}
